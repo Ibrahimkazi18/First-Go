e6cac683275a4a18467bfbf3b59be8f802fe8d8a
Ibrahim Kazi <ibirfkazi@gmail.com>
2025-01-01 17:16:45 +0530

    Authorizataion.Go

--- api/api.go
+++ api/api.go
@@ -1,20 +1,47 @@
 package api
 
-//Coins Balance Params
+import (
+	"encoding/json"
+	"net/http"
+)
+
+// Coins Balance Params
 type CoinsBalanceParams struct {
 	Username string
 }
 
-//Coins Balance Result
+// Coins Balance Result
 type CoinsBalanceResult struct {
 	Code int
 
 	Balance int64
 }
 
-//Error Result
-type ErrorResult struct {
+// Error Result
+type Error struct {
 	Code int
 
 	Message string
 }
+
+func writeError(w http.ResponseWriter, message string, code int) {
+	resp := Error{
+		Code:    code,
+		Message: message,
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+
+	json.NewEncoder(w).Encode(resp)
+}
+
+var (
+	RequestErrorHandler = func(w http.ResponseWriter, err error) {
+		writeError(w, err.Error(), http.StatusBadRequest)
+	}
+
+	InternalErrorHandler = func(w http.ResponseWriter) {
+		writeError(w, "An Unexpected Error Occured", http.StatusInternalServerError)
+	}
+)
--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -1,1 +1,19 @@
 package handler
+
+import (
+	"github.com/Ibrahimkazi18/First-Go/internal/middleware"
+	"github.com/go-chi/chi"
+	chimiddle "github.com/go-chi/chi/middleware"
+)
+
+func Handler(r *chi.Mux) {
+	r.Use(chimiddle.StripSlashes)
+
+	r.Route("/account", func(router chi.Router) {
+
+		//Middleware for /acount route
+		router.Use(middleware.Authorization)
+
+		// router.Get("/coins", GetCoinBalance)
+	})
+}
